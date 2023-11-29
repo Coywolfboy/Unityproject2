10e4ecf29063188f831fd723908c2e366d571306
EA7Air1 <omazarifa@gmail.com>
2023-11-29 21:23:43 +0100

    WebGL Publisher add

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,22 +6,41 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
     }
+
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
+
     public void Restart()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         SceneManager.LoadScene("Game");
     }
+
     public void Home(int sceneID)
     {
         Time.timeScale = 1f;
